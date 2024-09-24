c31e3ded7a02da72f0759f7ea54b27690fe9f68b
Vahnessa Gonzales <vahnessa.gonzales@gmail.com>
2024-09-24 20:05:39 +0800

    update as of 09-24

---
+++ script.js
@@ -0,0 +1,28 @@
+let sendBtn = document.getElementById("submit"); 
+
+
+function response() {
+    alert("We'll send you an email as soon as possible!")
+}
+
+sendBtn.addEventListener('click', response);
+
+let divquantityContainer = document.getElementById("quantity-container");
+let add = document.getElementById("add");
+let counter = document.getElementById("counter");
+
+function incrementOne() {
+    let parseToNumber = parseInt(counter.textContent);
+    parseToNumber += 1;
+    counter.textContent = parseToNumber;
+}
+
+add.addEventListener('click', incrementOne);
+
+function decrementOne() {
+    let parseToNumber = parseInt(counter.textContent);
+    parseToNumber -= 1;
+    counter.textContent = parseToNumber;
+}
+let minus = document.getElementById("minus");
+minus.addEventListener('click', decrementOne);
