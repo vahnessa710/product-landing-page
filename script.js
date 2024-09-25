a9a814812bd6d9f3541430f98154a588bb940bd1
Vahnessa Gonzales <vahnessa.gonzales@gmail.com>
2024-09-25 17:05:06 +0800

    update as of 09-25 5pm

--- script.js
+++ script.js
@@ -8,25 +8,47 @@ function response() {
 sendBtn.addEventListener('click', response);
 
 
-let add = document.getElementById("add");
-let counter = document.getElementById("counter");
+// let add = document.getElementById("add");
+// let counter = document.getElementById("counter");
 
+// function incrementOne() {
+//     let parseToNumber = parseInt(counter.textContent);
+//     parseToNumber += 1;
+//     counter.textContent = parseToNumber;
+// }
+
+// add.addEventListener('click', incrementOne);
+
+// function decrementOne() {
+//     let parseToNumber = parseInt(counter.textContent);
+//     parseToNumber -= 1;
+//     counter.textContent = parseToNumber;
+// }
+// let minus = document.getElementById("minus");
+// minus.addEventListener('click', decrementOne);
+
+// if (counter === 0) {
+//     decrementOne.disabled = true;
+// };
+
+const quantityInput = document.getElementById('quantity');
+const addButton = document.getElementById('add');
+const subtractButton = document.getElementById('subtract');
+
+// increasing button
 function incrementOne() {
-    let parseToNumber = parseInt(counter.textContent);
-    parseToNumber += 1;
-    counter.textContent = parseToNumber;
+    let currentQuantity = parseInt(quantityInput.value);
+    quantityInput.value = currentQuantity + 1;
 }
+addButton.addEventListener('click', incrementOne);
 
-add.addEventListener('click', incrementOne);
-
+//decreasing button
 function decrementOne() {
-    let parseToNumber = parseInt(counter.textContent);
-    parseToNumber -= 1;
-    counter.textContent = parseToNumber;
-}
-let minus = document.getElementById("minus");
-minus.addEventListener('click', decrementOne);
+    let currentQuantity = parseInt(quantityInput.value);
+    if (currentQuantity > 0) {
+        quantityInput.value = currentQuantity - 1;
+        };
+    }
+subtractButton.addEventListener('click', decrementOne);
 
-if (counter === 0) {
-    
-};
+// styles
