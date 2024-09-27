97c4c478d61eca7922ddcc3fe1d520f82ff28a2e
Vahnessa Gonzales <vahnessa.gonzales@gmail.com>
2024-09-27 23:56:21 +0800

    last update 09-27

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
+// send button
 let sendBtn = document.getElementById("submit"); 
-
+let emailBar = document.getElementById("email");
 
 function response() {
-    alert("We'll send you an email as soon as possible!")
+    if(emailBar.textContent === ""){
+    } else {
+        alert("We'll send you an email as soon as possible!");
 }
-
+}
 sendBtn.addEventListener('click', response);
 
+const quantityInput = document.getElementById('quantity');
+const addButton = document.getElementById('add');
+const subtractButton = document.getElementById('subtract');
 
-let add = document.getElementById("add");
-let counter = document.getElementById("counter");
-
+// increasing button
 function incrementOne() {
-    let parseToNumber = parseInt(counter.textContent);
-    parseToNumber += 1;
-    counter.textContent = parseToNumber;
+    let currentQuantity = parseInt(quantityInput.value);
+    quantityInput.value = currentQuantity + 1;
 }
+addButton.addEventListener('click', incrementOne);
 
-add.addEventListener('click', incrementOne);
-
+//decreasing button
 function decrementOne() {
-    let parseToNumber = parseInt(counter.textContent);
-    parseToNumber -= 1;
-    counter.textContent = parseToNumber;
-}
-let minus = document.getElementById("minus");
-minus.addEventListener('click', decrementOne);
+    let currentQuantity = parseInt(quantityInput.value);
+    if (currentQuantity > 0) {
+        quantityInput.value = currentQuantity - 1;
+        };
+    }
+subtractButton.addEventListener('click', decrementOne);
 
-if (counter === 0) {
-    
+
+// styles
+
+let checkoutBtn = document.getElementById("checkout-btn");
+checkoutBtn.addEventListener ('click', click);
+
+function click() {
+    const quantity = document.getElementById('quantity').value;
+    const responseDiv = document.getElementById('response-div');
+
+    if (quantity > 0) {
+        responseDiv.textContent = "Thank you for ordering!";
+    } else {
+        responseDiv.textContent = "Please add quantity.";
+    }
 };
